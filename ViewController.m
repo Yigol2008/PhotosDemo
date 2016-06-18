06037533179f26fe2ea3b5a9680487bb5caae9f6
Yigol <yigol2008@163.com>
2016-06-18 11:21:04 +0800

    Initial commit

---
+++ ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PhotosDemo
+//
+//  Created by Yigol on 16/5/10.
+//  Copyright © 2016年 Injoinow. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)takePhontos:(id)sender {
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
